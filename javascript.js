aa73708d412d64f9f4b17a91a7c0b088965f1a42
bonyoji <vdt.vincent.ta@gmail.com>
2023-12-09 20:00:20 +0900

    Add score system
    Added score tracker to playRound()
    Added results to game()

--- javascript.js
+++ javascript.js
@@ -21,40 +21,53 @@ function getPlayerChoice() {
 function playRound(playerSelection, computerSelection) {
     // Game is a tie, play again
     if (playerSelection === computerSelection){
+        console.log('Tie! Re-match!');
         return playRound(getPlayerChoice(), getComputerChoice());
     }
 
     // Compare results
     else if (playerSelection === `rock`){
         if (computerSelection === `paper`) {
+            computerScore++;
             return 'You Lose! Paper beats Rock!';
         } else {
+            playerScore++;
             return 'You Win! Rock beats Scissors!';
         }
     } else if (playerSelection === `paper`) {
         if (computerSelection === `scissors`) {
+            computerScore++;
             return 'You Lose! Scissors beat Paper!';
         } else {
+            playerScore++;
             return 'You Win! Paper beats Rock!';
         }
     } else if (playerSelection === `scissors`) {
         if (computerSelection === `rock`) {
+            computerScore++;
             return 'You Lose! Rock beats Scissors!';
         } else {
+            playerScore++;
             return 'You Win! Scissors beat Paper!';
         }
     }
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
+    // Repeat 5 times
     for (let i = 0; i < 5; i++ ) {
         console.log(playRound(getPlayerChoice(), getComputerChoice()));
     }
+    if (playerScore > computerScore) {
+        console.log('Congratulations! You Won!');
+    } else {
+        console.log('Too bad! You Lost!');
+    }
 }
 
-const playerSelection = getPlayerChoice();
+const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
+
 game();
