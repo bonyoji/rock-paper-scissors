af28ca8974588a576fca4d9661b4bd150359b922
bonyoji <vdt.vincent.ta@gmail.com>
2023-12-09 19:24:27 +0900

    Fixed code that determines winner
    Changed code to remove an extra run of getComputerChoice().
    Fixed syntax error on tie breaker line.

--- javascript.js
+++ javascript.js
@@ -3,15 +3,14 @@
 function getComputerChoice() {
     // Randomly choose an option between 'Rock', 'Paper, or 'Scissors'
     let randomNum = Math.floor(Math.random()*3);
-    if (randomNum == 1) {
+    if (randomNum === 1) {
         return `rock`;
-    } else if (randomNum == 2) {
+    } else if (randomNum === 2) {
         return `paper`;
     } else {
         return `scissors`;
     }
 }
-console.log(getComputerChoice());
 
 function getPlayerChoice() {
     // Get player choice
@@ -22,24 +21,33 @@ function getPlayerChoice() {
 function playRound(playerSelection, computerSelection) {
     // Game is a tie, play again
     if (playerSelection === computerSelection){
-        playAgain();
-    }
-
-    function playAgain() {
-        playRound(getPlayerChoice(), getComputerChoice());
+        return playRound(getPlayerChoice(), getComputerChoice());
     }
 
     // Compare results
-    if (playerSelection === `rock`){
+    else if (playerSelection === `rock`){
         if (computerSelection === `paper`) {
             return 'You Lose! Paper beats Rock!';
-        } else if (computerSelection === `scissors`) {
+        } else {
             return 'You Win! Rock beats Scissors!';
+        }
+    } else if (playerSelection === `paper`) {
+        if (computerSelection === `scissors`) {
+            return 'You Lose! Scissors beat Paper!';
+        } else {
+            return 'You Win! Paper beats Rock!';
+        }
+    } else if (playerSelection === `scissors`) {
+        if (computerSelection === `rock`) {
+            return 'You Lose! Rock beats Scissors!';
+        } else {
+            return 'You Win! Scissors beat Paper!';
+        }
     }
 }
 
-
-
-const playerSelection = 'getPlayerChoice()';
+const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
