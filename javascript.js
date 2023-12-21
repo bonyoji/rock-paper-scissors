bf747a6f995a2a679b69c73c24f9d3c6a592b704
bonyoji <vdt.vincent.ta@gmail.com>
2023-12-21 16:12:19 +0900

    Add match winner display
    Changed console outputs to display on ui

--- javascript.js
+++ javascript.js
@@ -12,52 +12,52 @@ function getComputerChoice() {
     }
 }
 
-// function getPlayerChoice() {
-//     // Get player choice
-//     let userChoice = prompt(`'Rock, Paper, or Scissors' ?`).toLowerCase();
-//     return userChoice;
-// }
+function getPlayerChoice(choice) {
+    return choice.toLowerCase();
+}
 
 const buttons = document.querySelectorAll('button');
+const scorePlayer = document.querySelector('#my_score');
+const scoreCPU = document.querySelector('#cpu_score');
+const gameResult = document.querySelector('#game_result');
+const matchResult = document.querySelector('#match_result');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        console.log('Clicked');
-        console.log(button.innerHTML.toLowerCase());
+        playRound(getPlayerChoice(button.innerHTML), getComputerChoice());
     });
 });
 
 function playRound(playerSelection, computerSelection) {
     // Game is a tie, play again
     if (playerSelection === computerSelection){
-        console.log('Tie! Re-match!');
-        return playRound('rock', getComputerChoice());
+        matchResult.textContent = 'Tie! Re-match!';
     }
 
     // Compare results
     else if (playerSelection === `rock`){
         if (computerSelection === `paper`) {
             computerScore++;
-            return 'You Lose! Paper beats Rock!';
+            matchResult.textContent = 'You Lose! Paper beats Rock!';
         } else {
             playerScore++;
-            return 'You Win! Rock beats Scissors!';
+            matchResult.textContent = 'You Win! Rock beats Scissors!';
         }
     } else if (playerSelection === `paper`) {
         if (computerSelection === `scissors`) {
             computerScore++;
-            return 'You Lose! Scissors beat Paper!';
+            matchResult.textContent = 'You Lose! Scissors beat Paper!';
         } else {
             playerScore++;
-            return 'You Win! Paper beats Rock!';
+            matchResult.textContent = 'You Win! Paper beats Rock!';
         }
     } else if (playerSelection === `scissors`) {
         if (computerSelection === `rock`) {
             computerScore++;
-            return 'You Lose! Rock beats Scissors!';
+            matchResult.textContent = 'You Lose! Rock beats Scissors!';
         } else {
             playerScore++;
-            return 'You Win! Scissors beat Paper!';
+            matchResult.textContent = 'You Win! Scissors beat Paper!';
         }
     }
 }
